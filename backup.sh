1ebb5a8e3a4e6ffe9c49024520a396cb29fb187d
AnderCorral <81016443+AnderCorral@users.noreply.github.com>
2024-11-19 16:09:59 +0100

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Definir las variables de directorios
-origen="/home/usuario/seguridad"
+origen="/home/usuario/seguridad/"
 destino_base="/var/tmp/Backups"
 fecha=$(date -d 'today' +"%d-%m-%Y")
 fechayer=$(date -d '-1 days' +"%d-%m-%Y")
-destino="$destino_base/$fecha"
+destino="$destino_base/$fecha/"
 destinoayer="$destino_base/$fechayer/"
 
 # Crear el directorio de destino si no existe
